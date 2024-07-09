f892ae809d28d1f33afc15422dd7a30a744845f6
Gregoire <gregoiremuller3@gmail.com>
2024-07-09 14:22:42 +0200

    [fix] graph

--- bin-app/front/src/components/Login/Login.js
+++ bin-app/front/src/components/Login/Login.js
@@ -5,6 +5,9 @@ import SnackbarAlert from '../SnackbarAlert/SnackbarAlert';
 import { CircularProgress } from '@mui/material';
 import UserIcon from '@mui/icons-material/Person';
 import KeyIcon from '@mui/icons-material/Key';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+
 const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
@@ -15,6 +18,7 @@ const Login = () => {
   const [snackbarMessage, setSnackbarMessage] = useState('');
   const [snackbarColor, setSnackbarColor] = useState('success');
   const [loading, setLoading] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
 
   const { login, register } = useContext(AuthContext);
   const navigate = useNavigate();
@@ -64,62 +68,67 @@ const Login = () => {
     setLoading(true);
     setTimeout(() => { //temps de chargement factice
       setLoading(false);
-      setRegisterMode(!registerMode); 
-      setInvalidCredentials(false);     
+      setRegisterMode(!registerMode);
+      setInvalidCredentials(false);
     }, 800);
   };
 
   return (
-    <div>
-      <div className="min-h-screen flex items-center justify-center">
-        <div className="bg-white p-8 rounded sm:shadow-md w-full max-w-sm">
-          <h2 className="text-2xl font-bold mb-6 text-center">{registerMode ? 'Inscription' : 'Se connecter'}</h2>
+    <>
+      <div className="min-h-screen flex flex-col items-center justify-center sm:shadow-md w-full">
+          <h2 className="text-2xl font-bold mb-4 text-center">{registerMode ? 'Inscription' : 'Se connecter'}</h2>
 
           {!loading ? (
-            <form onSubmit={registerMode ? handleRegisterSubmit : handleLoginSubmit} className="space-y-6">
-            <div>
-              <label htmlFor="username" className="block text-sm font-medium text-gray-700"></label>
-              <input
-                id="username"
-                name="username"
-                type="text"
-                value={username}
-                placeholder="Nom d'utilisateur"
-                onChange={(e) => setUsername(e.target.value)}
-                required
-                autoComplete="off"
-                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-green-900 focus:border-green-900 sm:text-sm"
-              />
-            </div>
-            <div>
-              <label htmlFor="password" className="block text-sm font-medium text-gray-700"></label>
-              <input
-                id="password"
-                name="password"
-                type="password"
-                value={password}
-                placeholder="Mot de passe"
-                onChange={(e) => setPassword(e.target.value)}
-                required
-                autoComplete="off"
-                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-green-900 focus:border-green-900 sm:text-sm"
-              />
-            </div>
-            <div>
-            <button
-              type="submit"
-              className={`w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white ${registerMode ? 'bg-indigo-600 hover:bg-indigo-700' : 'bg-green-900 hover:bg-green-1000'} focus:outline-none focus:ring-2 focus:ring-offset-2 ${registerMode ? 'focus:ring-indigo-500' : 'focus:ring-green-800'}`}
-            >
-              {registerMode ? 'S\'inscrire' : 'Se connecter'}
-            </button>
-            </div>
-          </form>
+            <form onSubmit={registerMode ? handleRegisterSubmit : handleLoginSubmit} className="space-y-6 w-9/12 sm:w-3/12">
+                <label htmlFor="username" className="block text-sm font-medium text-gray-700"></label>
+                <input
+                  id="username"
+                  name="username"
+                  type="text"
+                  value={username}
+                  placeholder="Nom d'utilisateur"
+                  onChange={(e) => setUsername(e.target.value)}
+                  required
+                  autoComplete="off"
+                  className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-green-900 focus:border-green-900 sm:text-sm transition-colors duration-300 ease-in-out"
+                />
+              <div className="relative">
+                <label htmlFor="password" className="block text-sm font-medium text-gray-700"></label>
+                <input
+                  id="password"
+                  name="password"
+                  type={showPassword ? "text" : "password"}
+                  value={password}
+                  placeholder="Mot de passe"
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                  autoComplete="off"
+                  className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-green-900 focus:border-green-900 sm:text-sm transition-colors duration-300 ease-in-out"
+                />
+                <button
+                  type="button"
+                  onClick={() => setShowPassword(!showPassword)}
+                  className="absolute right-2 top-1.5 cursor-pointer hover:text-gray-400"
+                >
+                  {showPassword ? <VisibilityOffIcon /> : <VisibilityIcon />}
+                </button>
+              </div>
+              <div>
+                <button
+                  type="submit"
+                  className={`w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white 
+                  ${registerMode ? 'bg-indigo-600 hover:bg-indigo-700' : 'bg-green-900 hover:bg-green-1000'} focus:outline-none focus:ring-2 focus:ring-offset-2 ${registerMode ? 'focus:ring-indigo-500' : 'focus:ring-green-800'}`}
+                >
+                  {registerMode ? 'S\'inscrire' : 'Se connecter'}
+                </button>
+              </div>
+            </form>
           ) : (
             <div className="flex justify-center">
               <CircularProgress />
             </div>
           )}
-          
+
           <div className="mt-4 text-sm text-center">
             {registerMode ? (
               <p>
@@ -143,7 +152,6 @@ const Login = () => {
               </p>
             )}
           </div>
-        </div>
       </div>
       <SnackbarAlert
         open={openSnackbar}
@@ -151,7 +159,7 @@ const Login = () => {
         message={snackbarMessage}
         color={snackbarColor}
       />
-    </div>
+    </>
   );
 };
 
